3d743d076dd1f0db62f622dbbaedf48c07e21428
Jiri Landsman <landsman@cngroup.dk>
2017-01-20 08:51:01 +0100

    test

--- deploy.cmd
+++ deploy.cmd
@@ -107,7 +107,7 @@ IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
 
 :: 4. Build application
 pushd "%DEPLOYMENT_TARGET%"
-call :ExecuteCmd ".\node_modules\.bin\react-scripts.cmd deploy"
+::call :ExecuteCmd ".\node_modules\.bin\react-scripts.cmd deploy"
 
 IF !ERRORLEVEL! NEQ 0 goto error
 popd
